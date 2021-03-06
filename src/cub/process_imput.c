b5dbe9f5acb05e5f5692b0d030b1f3a4d5fc929d
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-03-06 02:26:44 -0300

    improve process imput

--- src/cub/process_imput.c
+++ src/cub/process_imput.c
@@ -6,7 +6,7 @@
 /*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 23:06:00 by anolivei          #+#    #+#             */
-/*   Updated: 2021/03/05 23:41:10 by anolivei         ###   ########.fr       */
+/*   Updated: 2021/03/06 02:22:53 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,25 @@ int	process_imput(int keycode, t_all *all)
 	if (keycode == TAB_KEYM || keycode == TAB_KEYL)
 		render(all);
 	if (keycode == W_KEYM || keycode == W_KEYL)
-	{
-		all->player.y--;
-		render(all);
-	}
+		walk_turn_dir(all->player.walk_dir, +1, all);
 	if (keycode == S_KEYM || keycode == S_KEYL)
-	{
-		all->player.y++;
-		render(all);
-	}
-	if (keycode == A_KEYM || keycode == A_KEYL)
-	{
-		all->player.x--;
-		render(all);
-	}
+		walk_turn_dir(all->player.walk_dir, -1, all);
 	if (keycode == D_KEYM || keycode == D_KEYL)
-	{
-		all->player.x++;
-		render(all);
-	}
-/*	if (keycode == LEFT_ARROW_KEYM || keycode == LEFT_ARROW_KEYL)
-	{
-
-	}
-	if (keycode == W_KEYM || keycode == W_KEYL)
-	{
-
-	}*/
+		walk_turn_dir(all->player.turn_dir, +1, all);
+	if (keycode == A_KEYM || keycode == A_KEYL)
+		walk_turn_dir(all->player.turn_dir, -1, all);
 	if (keycode == ESC_KEYM || keycode == ESC_KEYL)
 	{
 		destroy_window(all);
 		return (FALSE);
 	}
 	return (TRUE);
+}
+
+void	walk_turn_dir(int walk_turn_dir, int i, t_all *all)
+{
+	walk_turn_dir = i;
+	render(all);
 }
 
 void	destroy_window(t_all *all)
