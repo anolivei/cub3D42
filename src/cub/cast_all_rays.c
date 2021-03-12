1a0e8f46a4caaf204a315046221f1515c85396ad
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-03-12 00:35:38 -0300

    starting calculate horiz and vert hit distances

--- src/cub/cast_all_rays.c
+++ src/cub/cast_all_rays.c
@@ -6,7 +6,7 @@
 /*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 21:00:12 by anolivei          #+#    #+#             */
-/*   Updated: 2021/03/11 00:41:30 by anolivei         ###   ########.fr       */
+/*   Updated: 2021/03/12 00:29:15 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,29 +53,29 @@ static void	setup_rays(t_intsc *hv)
 void		cast_ray(float ray_angle, int strip_id, t_all *all)
 {
 	ray_angle = normalize_angle(ray_angle);
-	all->intsc.is_ray_fac_down = ray_angle > 0 && ray_angle < PI;
-	all->intsc.is_ray_fac_up = !all->intsc.is_ray_fac_down;
-	all->intsc.is_ray_fac_right = ray_angle < 0.5 * PI || ray_angle > 1.5 * PI;
-	all->intsc.is_ray_fac_left = !all->intsc.is_ray_fac_right;
+	all->hor.is_ray_fac_down = ray_angle > 0 && ray_angle < PI;
+	all->hor.is_ray_fac_up = !all->hor.is_ray_fac_down;
+	all->hor.is_ray_fac_right = ray_angle < 0.5 * PI || ray_angle > 1.5 * PI;
+	all->hor.is_ray_fac_left = !all->hor.is_ray_fac_right;
 	/*
 	** Horizontal
 	*/
 	setup_rays(&all->hor);
-	all->intsc.y_interc = floor (all->player.y / TILE_SIZE * TILE_SIZE);
-	all->intsc.y_interc += all->intsc.is_ray_fac_down ? TILE_SIZE : 0;
-	all->intsc.x_interc = all->player.x + (all->intsc.y_interc - all->player.y) / tan(ray_angle);
-	all->intsc.y_step = TILE_SIZE;
-	all->intsc.y_step *= all->intsc.is_ray_fac_up ? -1 : 1;
-	all->intsc.x_step = TILE_SIZE / tan(ray_angle);
-	all->intsc.x_step *= (all->intsc.is_ray_fac_left && all->intsc.x_step > 0) ? -1 : 1;
-	all->intsc.x_step *= (all->intsc.is_ray_fac_right && all->intsc.x_step < 0) ? -1 : 1;
-	all->hor.next_touch_x = all->intsc.x_interc;
-	all->hor.next_touch_y = all->intsc.y_interc;
+	all->hor.y_interc = floor (all->player.y / TILE_SIZE) * TILE_SIZE;
+	all->hor.y_interc += all->hor.is_ray_fac_down ? TILE_SIZE : 0;
+	all->hor.x_interc = all->player.x + (all->hor.y_interc - all->player.y) / tan(ray_angle);
+	all->hor.y_step = TILE_SIZE;
+	all->hor.y_step *= all->hor.is_ray_fac_up ? -1 : 1;
+	all->hor.x_step = TILE_SIZE / tan(ray_angle);
+	all->hor.x_step *= (all->hor.is_ray_fac_left && all->hor.x_step > 0) ? -1 : 1;
+	all->hor.x_step *= (all->hor.is_ray_fac_right && all->hor.x_step < 0) ? -1 : 1;
+	all->hor.next_touch_x = all->hor.x_interc;
+	all->hor.next_touch_y = all->hor.y_interc;
 	while (all->hor.next_touch_x >= 0 && all->hor.next_touch_x <= WIN_WIDTH &&
 		all->hor.next_touch_y >= 0 && all->hor.next_touch_x <= WIN_HEIGHT)
 	{
 		all->hor.x_to_check = all->hor.next_touch_x;
-		all->hor.y_to_check = all->hor.next_touch_y + (all->intsc.is_ray_fac_up ? -1 : 0);
+		all->hor.y_to_check = all->hor.next_touch_y + (all->hor.is_ray_fac_up ? -1 : 0);
 		if (has_wall_at(all->hor.x_to_check, all->hor.y_to_check))
 		{
 			// found the wall hit
@@ -87,29 +87,36 @@ void		cast_ray(float ray_angle, int strip_id, t_all *all)
 		}
 		else
 		{
-			all->hor.next_touch_x += all->intsc.x_step;
-			all->hor.next_touch_y += all->intsc.y_step;
+			all->hor.next_touch_x += all->hor.x_step;
+			all->hor.next_touch_y += all->hor.y_step;
 		}
 	}
 	/*
 	** Vertical
 	*/
 	setup_rays(&all->ver);
-	all->intsc.y_interc = floor (all->player.y / TILE_SIZE * TILE_SIZE);
-	all->intsc.y_interc += all->intsc.is_ray_fac_down ? TILE_SIZE : 0;
-	all->intsc.x_interc = all->player.x + (all->intsc.y_interc - all->player.y) / tan(ray_angle);
-	all->intsc.y_step = TILE_SIZE;
-	all->intsc.y_step *= all->intsc.is_ray_fac_up ? -1 : 1;
-	all->intsc.x_step = TILE_SIZE / tan(ray_angle);
-	all->intsc.x_step *= (all->intsc.is_ray_fac_left && all->intsc.x_step > 0) ? -1 : 1;
-	all->intsc.x_step *= (all->intsc.is_ray_fac_right && all->intsc.x_step < 0) ? -1 : 1;
-	all->ver.next_touch_x = all->intsc.x_interc;
-	all->ver.next_touch_y = all->intsc.y_interc;
+
+	all->ver.x_interc = floor (all->player.x / TILE_SIZE) * TILE_SIZE;
+	all->ver.x_interc += all->ver.is_ray_fac_right ? TILE_SIZE : 0;
+
+	all->ver.y_interc = all->player.y + (all->ver.x_interc - all->player.x) * tan(ray_angle);
+
+	all->ver.x_step = TILE_SIZE;
+	all->ver.x_step *= all->ver.is_ray_fac_left ? -1 : 1;
+
+	all->ver.y_step = TILE_SIZE * tan(ray_angle);
+	all->ver.y_step *= (all->ver.is_ray_fac_up && all->ver.y_step > 0) ? -1 : 1;
+	all->ver.y_step *= (all->ver.is_ray_fac_down && all->ver.y_step < 0) ? -1 : 1;
+
+	all->ver.next_touch_x = all->ver.x_interc;
+	all->ver.next_touch_y = all->ver.y_interc;
+
 	while (all->ver.next_touch_x >= 0 && all->ver.next_touch_x <= WIN_WIDTH &&
 		all->ver.next_touch_y >= 0 && all->ver.next_touch_x <= WIN_HEIGHT)
 	{
-		all->ver.x_to_check = all->ver.next_touch_x;
-		all->ver.y_to_check = all->ver.next_touch_y + (all->intsc.is_ray_fac_up ? -1 : 0);
+		all->ver.x_to_check = all->ver.next_touch_x + (all->ver.is_ray_fac_left ? -1 : 0);
+		all->ver.y_to_check = all->ver.next_touch_y;
+		
 		if (has_wall_at(all->ver.x_to_check, all->ver.y_to_check))
 		{
 			// found the wall hit
@@ -121,8 +128,8 @@ void		cast_ray(float ray_angle, int strip_id, t_all *all)
 		}
 		else
 		{
-			all->ver.next_touch_x += all->intsc.x_step;
-			all->ver.next_touch_y += all->intsc.y_step;
+			all->ver.next_touch_x += all->ver.x_step;
+			all->ver.next_touch_y += all->ver.y_step;
 		}
 	}
 	strip_id = strip_id + 1 - 1;
