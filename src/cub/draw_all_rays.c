b4d8ae88ee01a10546cf8b4e18ad6559fda496e3
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-03-20 22:50:58 -0300

    is working draw rays with a little problem

--- src/cub/draw_all_rays.c
+++ src/cub/draw_all_rays.c
@@ -6,57 +6,77 @@
 /*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 16:22:17 by anolivei          #+#    #+#             */
-/*   Updated: 2021/03/20 20:08:39 by anolivei         ###   ########.fr       */
+/*   Updated: 2021/03/20 22:48:57 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub.h"
 
-static void	draw_ray_0_90(t_all *all, float x_init, float y_init, float x_end, float y_end, float angle_norm)
+static void	draw_ray_0_90(t_all *all, float x_init, float y_init, int size, float angle)
 {
+	float	x_end;
+	float	y_end;
+
+	x_end = x_init + cos(angle) * size * MINIMAP_SCALE_FACTOR;
+	y_end = y_init + sin(angle) * size * MINIMAP_SCALE_FACTOR;
 	all->img.addr = mlx_get_data_addr(all->img.img_ptr, &all->img.bpp,
 		&all->img.size_l, &all->img.endian);
 	while ((x_init <= x_end) && (y_init <= y_end))
 	{
 		put_pixel(&all->img, (int)x_init, (int)y_init, BLUE);
-		x_init = x_init + cos(angle_norm) * MINIMAP_SCALE_FACTOR;
-		y_init = y_init + sin(angle_norm) * MINIMAP_SCALE_FACTOR;
+		x_init = x_init + cos(angle) * 1 * MINIMAP_SCALE_FACTOR;
+		y_init = y_init + sin(angle) * 1 * MINIMAP_SCALE_FACTOR;
 	}
 }
 
-static void	draw_ray_90_180(t_all *all, float x_init, float y_init, float x_end, float y_end, float angle_norm)
+static void	draw_ray_90_180(t_all *all, float x_init, float y_init, int size, float angle)
 {
+	float	x_end;
+	float	y_end;
+
+	x_end = x_init + cos(angle) * size * MINIMAP_SCALE_FACTOR;
+	y_end = y_init + sin(angle) * size * MINIMAP_SCALE_FACTOR;
 	all->img.addr = mlx_get_data_addr(all->img.img_ptr, &all->img.bpp,
 		&all->img.size_l, &all->img.endian);
 	while ((x_init >= x_end) && (y_init <= y_end))
 	{
 		put_pixel(&all->img, (int)x_init, (int)y_init, BLUE);
-		x_init = x_init + cos(angle_norm) * MINIMAP_SCALE_FACTOR;
-		y_init = y_init + sin(angle_norm) * MINIMAP_SCALE_FACTOR;
+		x_init = x_init + cos(angle) * 1 * MINIMAP_SCALE_FACTOR;
+		y_init = y_init + sin(angle) * 1 * MINIMAP_SCALE_FACTOR;
 	}
 }
 
-static void	draw_ray_180_270(t_all *all, float x_init, float y_init, float x_end, float y_end, float angle_norm)
+static void	draw_ray_180_270(t_all *all, float x_init, float y_init, int size, float angle)
 {
+	float	x_end;
+	float	y_end;
+
+	x_end = x_init + cos(angle) * size * MINIMAP_SCALE_FACTOR;
+	y_end = y_init + sin(angle) * size * MINIMAP_SCALE_FACTOR;
 	all->img.addr = mlx_get_data_addr(all->img.img_ptr, &all->img.bpp,
 		&all->img.size_l, &all->img.endian);
 	while ((x_init >= x_end) && (y_init >= y_end))
 	{
 		put_pixel(&all->img, (int)x_init, (int)y_init, BLUE);
-		x_init = x_init + cos(angle_norm) * MINIMAP_SCALE_FACTOR;
-		y_init = y_init + sin(angle_norm) * MINIMAP_SCALE_FACTOR;
+		x_init = x_init + cos(angle) * 1 * MINIMAP_SCALE_FACTOR;
+		y_init = y_init + sin(angle) * 1 * MINIMAP_SCALE_FACTOR;
 	}
 }
 
-static void	draw_ray_270_360(t_all *all, float x_init, float y_init, float x_end, float y_end, float angle_norm)
+static void	draw_ray_270_360(t_all *all, float x_init, float y_init, float size, float angle)
 {
+	float	x_end;
+	float	y_end;
+
+	x_end = x_init + cos(angle) * size * MINIMAP_SCALE_FACTOR;
+	y_end = y_init + sin(angle) * size * MINIMAP_SCALE_FACTOR;
 	all->img.addr = mlx_get_data_addr(all->img.img_ptr, &all->img.bpp,
 		&all->img.size_l, &all->img.endian);
 	while ((x_init <= x_end) && (y_init >= y_end))
 	{
 		put_pixel(&all->img, (int)x_init, (int)y_init, BLUE);
-		x_init = x_init + cos(angle_norm) * MINIMAP_SCALE_FACTOR;
-		y_init = y_init + sin(angle_norm) * MINIMAP_SCALE_FACTOR;
+		x_init = x_init + cos(angle) * 1 * MINIMAP_SCALE_FACTOR;
+		y_init = y_init + sin(angle) * 1 * MINIMAP_SCALE_FACTOR;
 	}
 }
 
@@ -64,21 +84,17 @@ void		draw_all_rays(t_all *all, int i)
 {
 	float	x_init;
 	float	y_init;
-	float	x_end;
-	float	y_end;
 	float	angle_norm;
 
 	x_init = all->player.x * MINIMAP_SCALE_FACTOR;
 	y_init = all->player.y * MINIMAP_SCALE_FACTOR;
-	x_end = all->ray[i].wall_hit_x * MINIMAP_SCALE_FACTOR;
-	y_end = all->ray[i].wall_hit_y * MINIMAP_SCALE_FACTOR;
 	angle_norm = normalize_angle(all->ray[i].ray_angle);
 	if (angle_norm >= 0 && angle_norm <= (90 * (PI / 180)))
-		draw_ray_0_90(all, x_init, y_init, x_end, y_end, angle_norm);
+		draw_ray_0_90(all, x_init, y_init, all->ray[i].distance, angle_norm);
 	if (angle_norm > (90 * (PI / 180)) && angle_norm <= (180 * (PI / 180)))
-		draw_ray_90_180(all, x_init, y_init, x_end, y_end, angle_norm);
+		draw_ray_90_180(all, x_init, y_init, all->ray[i].distance, angle_norm);
 	if (angle_norm > (180 * (PI / 180)) && angle_norm <= (270 * (PI / 180)))
-		draw_ray_180_270(all, x_init, y_init, x_end, y_end, angle_norm);
+		draw_ray_180_270(all, x_init, y_init, all->ray[i].distance, angle_norm);
 	if (angle_norm > (270 * (PI / 180)) && angle_norm <= (360 * (PI / 180)))
-		draw_ray_270_360(all, x_init, y_init, x_end, y_end, angle_norm);
+		draw_ray_270_360(all, x_init, y_init, all->ray[i].distance, angle_norm);
 }
